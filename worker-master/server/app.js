80129d43d3188a93f9d708d1f1da463e63a3e716
Jake Anderson <Maceto@dyn-169-226-224-238.wireless.albany.edu>
2019-11-21 22:04:43 -0500

    added real ICMP

--- worker-master/server/app.js
+++ worker-master/server/app.js
@@ -8,28 +8,46 @@ app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
+//Services deps
+var ping = require ("net-ping");
+
+var options = {
+    networkProtocol: ping.NetworkProtocol.IPv4,
+    packetSize: 16,
+    retries: 1,
+    sessionId: (process.pid % 65535),
+    timeout: 2000,
+    ttl: 128
+};
+
+var session = ping.createSession (options);
+
 // const mongodb_conn_module = require('./mongodbConnModule');
 // var db = mongodb_conn_module.connect();
 
 //dbdepends
 var Team = require("./models/team")
 
-console.log('Hello');
+//middleware if we get that far
+// const ICMP = require('./middleware/ICMP')
 
-const ICMP = require('./middleware/ICMP')
-
-var tIn = '8.8.8.8'
-
-app.use('/test', (req, res, next) => {
-    var test = ICMP.ICMP_CHECK(tIn)
-    res.send({
-        out: test
-    })
+app.get('/test/:ipIn', (req, res, next) => {
+    var ipIn = req.params.ipIn;
+    // console.log('ipAddr = ' + ipIn)
+    var target = ipIn;
+        var output = '';
+        session.pingHost (target, function (error, target, sent, rcvd) {
+            var ms = rcvd - sent;
+            if (error){
+                //output = target.toString() +  ": " + error.toString ();
+                console.log (target + ": " + error.toString ());
+            }
+            else{
+                //output = target.toString() +  ": Alive (ms=" + ms.toString() + ")";
+                console.log (target + ": Alive (ms=" + ms + ")");
+            }
+        });
 })
 
-var test = ICMP.ICMP_CHECK(tIn)
-
-console.log('working');
-console.log(test);
 
 app.listen(process.env.PORT || 8081)
