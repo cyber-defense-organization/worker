b85bec9d0e2daa14c86c5b89438f5a63daf37c7b
Jake Anderson <Maceto@dyn-169-226-81-153.wireless.albany.edu>
2019-11-24 15:38:35 -0500

    Added correct ICMP database functions

--- worker-master/server/app.js
+++ worker-master/server/app.js
@@ -8,6 +8,22 @@ app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
+//Team IP Info
+var teamIps = [
+    '8.8.8.8',
+    '8.8.8.9',
+    '8.8.8.8',
+    '8.8.8.8',
+    '8.8.8.8',
+]
+var boxNames = [
+    'Linux1',
+    'Linux2',
+    'Windows1',
+    'Windows2',
+    '98',
+]
+
 //Services deps
 
 //ICMP
@@ -123,25 +139,318 @@ app.get('/addICMP/:status/:name' , (req,res,next) => {
         });
 })
 
-app.get('/ICMP/:host', async (req, res, next) => {
-    var hostIn = req.params.host;
-    // console.log('ipAddr = ' + ipIn)
-    var target = hostIn;
+app.get('/ICMP_ALLo/:teamName' , async (req, res, next) =>{
+    //var hostIn = req.params.host;
+    var name = req.params.teamName;
+    var epochTime = Date.now();
+
+    for (let index = 0; index < teamIps.length; index++) {
+        var hostIn = teamIps[index];
+        const boxName = boxNames[index];
+    }
+
+    var result = await session.pingHost(hostIn, function (error, hostIn, sent, rcvd) {
+        var ms = rcvd - sent;
+        console.log(typeof db_index)
+        if (error){
+            var output = hostIn + ": " + error.toString()
+            Team.findOneAndUpdate(
+                { name: name }, 
+                {$push: {db_index :{ timeStamp: epochTime , status: false , error: error.toString()} }},
+                function(err,suc){
+                    if(err){
+                        console.log(err)
+                    }
+                    else{
+                        console.log(hostIn + ' : ' + db_index + " : " + output)
+                    }
+                });
+        }
+        else{
+            var output = hostIn + ": Alive (ms=" + ms + ")"
+            Team.findOneAndUpdate(
+                { name: name }, 
+                {$push: {db_index :{ timeStamp: epochTime , status: true , speed: ms} }},
+                function(err,suc){
+                    if(err){
+                        console.log(err)
+                    }else{
+                        console.log(hostIn + ' : ' + db_index + " : " + output)
+                    }
+                });
+        }
+    });
   
-    const result = await session.pingHost(target, function (error, target, sent, rcvd) {
+    // This for loop needs fixin
+    
+    for (let index = 0; index < teamIps.length; index++) {
+        var hostIn = teamIps[index];
+        const boxName = boxNames[index];
+        const liTest = 'services.0.ICMP_Linux1'
+        var db_base = 'services.0.ICMP_';
+        var db_index = db_base.concat(boxName);
+        console.log('Inital : ' + boxName)
+
+        //This should work and makes life easier but I cant append the db_index string into the mongoose push call
+
+        var result = await session.pingHost(hostIn, function (error, hostIn, sent, rcvd) {
+            var ms = rcvd - sent;
+            var db_base = 'services.0.ICMP_';
+            var db_index = db_base.concat(boxName).toString();
+            console.log(typeof db_index)
+            if (error){
+                var output = hostIn + ": " + error.toString()
+                Team.findOneAndUpdate(
+                    { name: name }, 
+                    {$push: {db_index :{ timeStamp: epochTime , status: false , error: error.toString()} }},
+                    function(err,suc){
+                        if(err){
+                            console.log(err)
+                        }
+                        else{
+                            console.log(hostIn + ' : ' + db_index + " : " + output)
+                        }
+                    });
+                // res.send({
+                //     result: output
+                // })
+            }
+            else{
+                var output = hostIn + ": Alive (ms=" + ms + ")"
+                Team.findOneAndUpdate(
+                    { name: name }, 
+                    {$push: {db_index :{ timeStamp: epochTime , status: true , speed: ms} }},
+                    function(err,suc){
+                        if(err){
+                            console.log(err)
+                        }else{
+                            console.log(hostIn + ' : ' + db_index + " : " + output)
+                        }
+                    });
+                // res.send({
+                //     result: output
+                // })
+            }
+        });
+    }
+
+    res.send({
+        debug: 'This works'
+    })
+})
+
+app.get('/ICMP_ALL/:teamName', async (req,res,next) => {
+    var name = req.params.teamName;
+    var epochTime = Date.now();
+    
+    var hostIn1 = teamIps[0];
+    var hostIn2 = teamIps[1];
+    var hostIn3 = teamIps[2];
+    var hostIn4 = teamIps[3];
+    var hostIn5 = teamIps[4];
+
+    console.log('t1')
+
+    //Call 1
+    var result = await session.pingHost(hostIn1, function (error, hostIn1, sent, rcvd) {
         var ms = rcvd - sent;
         if (error){
-            //output = target.toString() +  ": " + error.toString ();
-            console.log (target + ": " + error.toString ());
-            var output = target + ": " + error.toString ()
+            var output = hostIn1 + ": " + error.toString()
+            Team.findOneAndUpdate(
+                { name: name }, 
+                {$push: {'services.0.ICMP_Linux1' :{ timeStamp: epochTime , status: false , error: error.toString()} }},
+                function(err,suc){
+                    if(err){
+                        console.log(err)
+                    }
+                    else{
+                        //console.log(hostIn1 + ' : ' + db_index + " : " + output)
+                    }
+                });
+        }
+        else{
+            var output = hostIn1 + ": Alive (ms=" + ms + ")"
+            Team.findOneAndUpdate(
+                { name: name }, 
+                {$push: {'services.0.ICMP_Linux1' :{ timeStamp: epochTime , status: true , speed: ms} }},
+                function(err,suc){
+                    if(err){
+                        console.log(err)
+                    }else{
+                        //console.log(hostIn1 + ' : ' + db_index + " : " + output)
+                    }
+                });
+        }
+    });
+
+    //Call 2
+    var result = await session.pingHost(hostIn2, function (error, hostIn2, sent, rcvd) {
+        var ms = rcvd - sent;
+        if (error){
+            var output = hostIn2 + ": " + error.toString()
+            Team.findOneAndUpdate(
+                { name: name }, 
+                {$push: {'services.0.ICMP_Linux2' :{ timeStamp: epochTime , status: false , error: error.toString()} }},
+                function(err,suc){
+                    if(err){
+                        console.log(err)
+                    }
+                    else{
+                        //console.log(hostIn2 + ' : ' + db_index + " : " + output)
+                    }
+                });
+        }
+        else{
+            var output = hostIn2 + ": Alive (ms=" + ms + ")"
+            Team.findOneAndUpdate(
+                { name: name }, 
+                {$push: {'services.0.ICMP_Linux2' :{ timeStamp: epochTime , status: true , speed: ms} }},
+                function(err,suc){
+                    if(err){
+                        console.log(err)
+                    }else{
+                        //console.log(hostIn2 + ' : ' + db_index + " : " + output)
+                    }
+                });
+        }
+    });
+
+    //Call 3
+    var result = await session.pingHost(hostIn3, function (error, hostIn3, sent, rcvd) {
+        var ms = rcvd - sent;
+        if (error){
+            var output = hostIn3 + ": " + error.toString()
+            Team.findOneAndUpdate(
+                { name: name }, 
+                {$push: {'services.0.ICMP_Windows1' :{ timeStamp: epochTime , status: false , error: error.toString()} }},
+                function(err,suc){
+                    if(err){
+                        console.log(err)
+                    }
+                    else{
+                        //console.log(hostIn3 + ' : ' + db_index + " : " + output)
+                    }
+                });
+        }
+        else{
+            var output = hostIn3 + ": Alive (ms=" + ms + ")"
+            Team.findOneAndUpdate(
+                { name: name }, 
+                {$push: {'services.0.ICMP_Windows1' :{ timeStamp: epochTime , status: true , speed: ms} }},
+                function(err,suc){
+                    if(err){
+                        console.log(err)
+                    }else{
+                        //console.log(hostIn3 + ' : ' + db_index + " : " + output)
+                    }
+                });
+        }
+    });
+
+    //Call 4
+    var result = await session.pingHost(hostIn4, function (error, hostIn4, sent, rcvd) {
+        var ms = rcvd - sent;
+        if (error){
+            var output = hostIn4 + ": " + error.toString()
+            Team.findOneAndUpdate(
+                { name: name }, 
+                {$push: {'services.0.ICMP_Windows2' :{ timeStamp: epochTime , status: false , error: error.toString()} }},
+                function(err,suc){
+                    if(err){
+                        console.log(err)
+                    }
+                    else{
+                        //console.log(hostIn4 + ' : ' + db_index + " : " + output)
+                    }
+                });
+        }
+        else{
+            var output = hostIn4 + ": Alive (ms=" + ms + ")"
+            Team.findOneAndUpdate(
+                { name: name }, 
+                {$push: {'services.0.ICMP_Windows2' :{ timeStamp: epochTime , status: true , speed: ms} }},
+                function(err,suc){
+                    if(err){
+                        console.log(err)
+                    }else{
+                        //console.log(hostIn4 + ' : ' + db_index + " : " + output)
+                    }
+                });
+        }
+    });
+
+    //Call 5
+    var result = await session.pingHost(hostIn5, function (error, hostIn5, sent, rcvd) {
+        var ms = rcvd - sent;
+        if (error){
+            var output = hostIn5 + ": " + error.toString()
+            Team.findOneAndUpdate(
+                { name: name }, 
+                {$push: {'services.0.ICMP_98' :{ timeStamp: epochTime , status: false , error: error.toString()} }},
+                function(err,suc){
+                    if(err){
+                        console.log(err)
+                    }
+                    else{
+                        //console.log(hostIn5 + ' : ' + db_index + " : " + output)
+                    }
+                });
+        }
+        else{
+            var output = hostIn5 + ": Alive (ms=" + ms + ")"
+            Team.findOneAndUpdate(
+                { name: name }, 
+                {$push: {'services.0.ICMP_98' :{ timeStamp: epochTime , status: true , speed: ms} }},
+                function(err,suc){
+                    if(err){
+                        console.log(err)
+                    }else{
+                        //console.log(hostIn5 + ' : ' + db_index + " : " + output)
+                    }
+                });
+        }
+    });
+    res.send({
+        msg: 'Jank version but it works'
+    })
+
+    
+})
+app.get('/ICMP/:host/:teamName', async (req, res, next) => {
+    var hostIn = req.params.host;
+    var name = req.params.teamName;
+    var epochTime = Date.now();
+  
+    const result = await session.pingHost(hostIn, function (error, hostIn, sent, rcvd) {
+        var ms = rcvd - sent;
+        if (error){
+            // output = target.toString() +  ": " + error.toString ();
+            // console.log (hostIn + ": " + error.toString ());
+            var output = hostIn + ": " + error.toString()
+            Team.findOneAndUpdate(
+                { name: name }, 
+                {$push: {'services.0.ICMP':{ timeStamp: epochTime , status: false , error: error.toString()} }},
+                function(err,suc){
+                    if(err){
+                        console.log(err)
+                    }
+                });
             res.send({
                 result: output
             })
         }
         else{
-            //output = target.toString() +  ": Alive (ms=" + ms.toString() + ")";
-            console.log (target + ": Alive (ms=" + ms + ")");
-            var output = target + ": Alive (ms=" + ms + ")"
+            // output = target.toString() +  ": Alive (ms=" + ms.toString() + ")";
+            // console.log (hostIn + ": Alive (ms=" + ms + ")");
+            var output = hostIn + ": Alive (ms=" + ms + ")"
+            Team.findOneAndUpdate(
+                { name: name }, 
+                {$push: {'services.0.ICMP':{ timeStamp: epochTime , status: true , speed: ms} }},
+                function(err,suc){
+                    if(err){
+                        console.log(err)
+                    }
+                });
             res.send({
                 result: output
             })
--- worker-master/server/models/team.js
+++ worker-master/server/models/team.js
@@ -5,10 +5,42 @@ var teamSchema = new Schema({
      name: String,
      score: Number,
      services: 
-          [{
-               ICMP: [{
-                    timeStamp: Number,
-                    status: Boolean //Future add up and down
+          [{   
+               // ICMP: [{
+               //      timeStamp: {type: Date},
+               //      status: {type: Boolean, default: false},
+               //      error: String,
+               //      speed: String
+               // }],
+               ICMP_Linux1: [{
+                    timeStamp: {type: Date},
+                    status: {type: Boolean, default: false},
+                    error: String,
+                    speed: String
+               }],
+               ICMP_Linux2: [{
+                    timeStamp: {type: Date},
+                    status: {type: Boolean, default: false},
+                    error: String,
+                    speed: String
+               }],
+               ICMP_Windows1: [{
+                    timeStamp: {type: Date},
+                    status: {type: Boolean, default: false},
+                    error: String,
+                    speed: String
+               }],
+               ICMP_Windows2: [{
+                    timeStamp: {type: Date},
+                    status: {type: Boolean, default: false},
+                    error: String,
+                    speed: String
+               }],
+               ICMP_98: [{
+                    timeStamp: {type: Date},
+                    status: {type: Boolean, default: false},
+                    error: String,
+                    speed: String
                }],
                
           }]
